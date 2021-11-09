f5d8287b9c441ee89e59b02c8cad44b87776b63a
Kieran Rajasansir <kieran@rajasansir.co.uk>
2021-11-09 18:23:11 +0000

    added nav bar and placeholders for start and end dates

--- src/app/expenses/full-data-history/full-data-history.component.ts
+++ src/app/expenses/full-data-history/full-data-history.component.ts
@@ -8,12 +8,14 @@ import { TransactionsService } from '../transaction.service';
 export class FullDataHistoryComponent implements OnInit {
   pageTitle: string = 'Historic Data';
   transactionData: any = [];
+  total: number = 0;
 
   constructor(private transactionService: TransactionsService) {}
 
   ngOnInit(): void {
     this.transactionService.getAllTransactions().subscribe((results) => {
-      this.transactionData = results.data;
+      this.transactionData = results.data.transactions;
+      this.total = results.data.total;
     });
   }
 
