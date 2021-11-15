a25e5af67451aa447bd3c4d2b00495d2e20e8c4b
Kieran <kieran@rajasansir.co.uk>
2021-11-15 14:22:37 +0000

    removed home link from nav-bar on homepage

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent implements OnInit {
+  @Input()
+  hideHomeLink: boolean = false
+  navLinkClass: string = ''
 
   constructor() { }
 
   ngOnInit(): void {
+    if (this.hideHomeLink) {
+      this.navLinkClass = 'hideHomeLink'
+    }
   }
 
 }
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
   pageTitle: string = 'Home';
+  hideHomeLink: boolean = true
 
   constructor() {}
 
